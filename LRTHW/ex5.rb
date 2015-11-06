54be10e569ea2f18a1098d2fed871d73b07075dc
chris <chrissiegman@yahoo.com>
2015-11-06 11:56:48 -0600

    Add exercises 1-7 of LRTHW

---
+++ LRTHW/ex5.rb
@@ -0,0 +1,22 @@
+my_name = 'Chris Siegman'
+my_age = 27
+my_height = 74 #inches
+my_weight = 203 #lbs
+my_eyes = 'Blue'
+my_teeth = 'Whitish'
+my_hair = 'Nil'
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} lbs heavy."
+puts "Actually that's a bit heavy."
+puts "He's got #{my_eyes} colored eyes."
+puts "His teeth are #{my_teeth} in color."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+
+puts 'Does it work in here? I am #{my_age} years old.'
+
+
+
+
