c9ba2f6718238ecb36b0002e0eaa7d834697228c
chris <chrissiegman@yahoo.com>
2015-11-08 17:40:44 -0600

    Finish Euler 3!

---
+++ project_euler/problem_3.rb
@@ -0,0 +1,39 @@
+# Find the biggest prime factor of 600851475143
+# This is not an optimal solution but it works for this number
+require 'prime'
+
+# make a list of primes that we "hope" is big enough
+$primes = Prime.take(1000)
+
+$n = 600851475143 
+$factors = []
+
+
+def filter_prime_factors(number)
+  for prime in $primes
+    if $n % prime == 0
+      $n = $n / prime
+      $factors.push(prime)
+    end
+  end
+end
+
+def factor
+  100.times do
+    filter_prime_factors($n)
+  end
+end
+
+if $n != 1
+  factor
+else
+  puts "done"
+end
+
+puts "new_number: #{$n}"
+puts "factors: #{$factors}"
+
+
+
+
+
