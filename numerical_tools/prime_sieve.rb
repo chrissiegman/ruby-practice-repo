cabe3e4d870c8aa2b99360dd8e6f12dcb9eea0c4
chris <chrissiegman@yahoo.com>
2015-11-08 21:33:34 -0600

    Made prime sieve more idiomatic and flexible

--- numerical_tools/prime_sieve.rb
+++ numerical_tools/prime_sieve.rb
@@ -1,12 +1,21 @@
 # Based of the Sieve of Eratosthenes
+# Finds all primes less than or equal to n
 
-num_list = (2..100).to_a
-$prime_list = []
+upper_bound = ARGV.first.to_i
 
-while !num_list.empty?
-  prime = num_list.first
-  $prime_list << num_list.first
-  num_list = num_list.delete_if{|x| x % prime == 0 }
+def get_primes(upper_bound)
+  num_list = (2..upper_bound).to_a
+  prime_list = []
+
+  loop do
+    prime = num_list.first
+    prime_list << num_list.first
+    num_list = num_list.delete_if{|x| x % prime == 0 }
+    break if num_list.empty?
+  end
+  return prime_list
+
 end
 
-puts $prime_list
+print get_primes(upper_bound)
+print "\n"
